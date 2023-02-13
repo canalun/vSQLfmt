711fd4a8b42a3d2e5aaa930fa47bb272c23500ab
Kanaru Sato <kanakanarunrun@gmail.com>
2023-02-14 02:46:25 +0900

    support queries other than INSERT between INSERT queries(just ignore, not format)

--- src/extension.ts
+++ src/extension.ts
@@ -19,7 +19,9 @@ export function activate(context: vscode.ExtensionContext) {
       // keep comments
       const queriesAndComments = text
         .replace(/\n/g, '')
-        .split(/(?=INSERT INTO )|(?=--)/g)
+        .split(
+          /(?=INSERT INTO)|(?=SELECT \* FROM)|(?=CREATE TABLE)|(?=DELETE FROM)|(?=UPDATE )|(?=ALTER TABLE)|(?=--)/g
+        )
       const commentsMap: Map<number, string> = new Map()
       queriesAndComments.forEach((str, i) => {
         str.startsWith('--') && commentsMap.set(i, str)
@@ -36,97 +38,93 @@ export function activate(context: vscode.ExtensionContext) {
       const charLengthsOfEachColumnOfEachQuery: Array<Array<Array<number>>> = [] // [queryIndex][ColumnIndex][Record(incl.ColumnNameReference)Index]
       for (let i = 0; i < asts.length; i++) {
         const query = asts[i]
-        if (query.type !== 'insert' || !query.columns) {
-          vscode.window.showInformationMessage(
-            'failed: query types other than "INSERT" are not supported yet...!',
-            {
-              modal: true,
-            }
-          )
-          return
-        }
+
         const charLengthsOfEachColumn: Array<Array<number>> = [] // [ColumnIndex][Record(incl.ColumnNameReference)Index]
         const maxLengthsOfEachColumn: Array<number> = [] // [ColumnIndex]
-        for (
-          let columnIndex = 0;
-          columnIndex < query.columns.length;
-          columnIndex++
-        ) {
-          const columnName = query.columns[columnIndex]
-          const charLengthsOfColumn: Array<number> = [] // [Record(incl.ColumnNameReference)Index]
-          charLengthsOfColumn.push(columnName.length)
-          let maxLengthOfColumn = columnName.length
-          query.values.forEach(
-            (value: {
-              type: 'expr_list'
-              // FIXME: the below 'value' type is just rule of thumb and so uncertain.
-              // this is because we've not found the clear type definition
-              // used by florajs/sql-parser yet.
-              // And this uncertainty can cause runtime error.
-              // Currently, the former is chosen except for the case
-              // in which back-quotation is used.
-              value: Array<
-                | {
-                    type: 'number' | 'bool' | 'null' | string
-                    value: string
-                  }
-                | { type: 'column_ref'; column: string; table: null | string } // when back-quotations are used as delimiter in value clauses
-                | { type: 'function'; name: string }
-              >
-            }) => {
-              let charLength = 0
-              const _value = value.value[columnIndex]
-              switch (_value.type) {
-                case 'number':
-                case 'bool':
-                case 'null':
-                  // most values have two delimiters(e.g. column name: back quotation; function: (); etc.).
-                  // but these three kinds of values don't have delimiters.
-                  // that's why magic number "2" is subtracted here.
-                  charLength = String(_value.value).length - 2
-                  break
-                case 'column_ref':
-                  if (!('column' in _value)) {
-                    vscode.window.showInformationMessage(
-                      "failed: couldn't parse the file...!",
-                      {
-                        modal: true,
-                      }
-                    )
-                    return
-                  }
-                  charLength = String(_value.column).length
-                  break
-                case 'function':
-                  if (!('name' in _value)) {
-                    vscode.window.showInformationMessage(
-                      "failed: couldn't parse the file...!",
-                      {
-                        modal: true,
-                      }
-                    )
-                    return
-                  }
-                  charLength = String(_value.name).length
-                  break
-                default:
-                  if (!('value' in _value)) {
-                    vscode.window.showInformationMessage(
-                      "failed: couldn't parse the file...!",
-                      {
-                        modal: true,
-                      }
-                    )
-                    return
-                  }
-                  charLength = String(_value.value).length // FIXME
+
+        // if query is other than INSERT, skip constructing the two arrays.
+        if (query.type === 'insert' && query.columns) {
+          for (
+            let columnIndex = 0;
+            columnIndex < query.columns.length;
+            columnIndex++
+          ) {
+            const columnName = query.columns[columnIndex]
+            const charLengthsOfColumn: Array<number> = [] // [Record(incl.ColumnNameReference)Index]
+            charLengthsOfColumn.push(columnName.length)
+            let maxLengthOfColumn = columnName.length
+            query.values.forEach(
+              (value: {
+                type: 'expr_list'
+                // FIXME: the below 'value' type is just rule of thumb and so uncertain.
+                // this is because we've not found the clear type definition
+                // used by florajs/sql-parser yet.
+                // And this uncertainty can cause runtime error.
+                // Currently, the former is chosen except for the case
+                // in which back-quotation is used.
+                value: Array<
+                  | {
+                      type: 'number' | 'bool' | 'null' | string
+                      value: string
+                    }
+                  | { type: 'column_ref'; column: string; table: null | string } // when back-quotations are used as delimiter in value clauses
+                  | { type: 'function'; name: string }
+                >
+              }) => {
+                let charLength = 0
+                const _value = value.value[columnIndex]
+                switch (_value.type) {
+                  case 'number':
+                  case 'bool':
+                  case 'null':
+                    // most values have two delimiters(e.g. column name: back quotation; function: (); etc.).
+                    // but these three kinds of values don't have delimiters.
+                    // that's why magic number "2" is subtracted here.
+                    charLength = String(_value.value).length - 2
+                    break
+                  case 'column_ref':
+                    if (!('column' in _value)) {
+                      vscode.window.showInformationMessage(
+                        "failed: couldn't parse the file...!",
+                        {
+                          modal: true,
+                        }
+                      )
+                      return
+                    }
+                    charLength = String(_value.column).length
+                    break
+                  case 'function':
+                    if (!('name' in _value)) {
+                      vscode.window.showInformationMessage(
+                        "failed: couldn't parse the file...!",
+                        {
+                          modal: true,
+                        }
+                      )
+                      return
+                    }
+                    charLength = String(_value.name).length
+                    break
+                  default:
+                    if (!('value' in _value)) {
+                      vscode.window.showInformationMessage(
+                        "failed: couldn't parse the file...!",
+                        {
+                          modal: true,
+                        }
+                      )
+                      return
+                    }
+                    charLength = String(_value.value).length
+                }
+                charLengthsOfColumn.push(charLength)
+                maxLengthOfColumn = Math.max(charLength, maxLengthOfColumn)
               }
-              charLengthsOfColumn.push(charLength)
-              maxLengthOfColumn = Math.max(charLength, maxLengthOfColumn)
-            }
-          )
-          charLengthsOfEachColumn.push(charLengthsOfColumn)
-          maxLengthsOfEachColumn.push(maxLengthOfColumn)
+            )
+            charLengthsOfEachColumn.push(charLengthsOfColumn)
+            maxLengthsOfEachColumn.push(maxLengthOfColumn)
+          }
         }
         charLengthsOfEachColumnOfEachQuery.push(charLengthsOfEachColumn)
         maxCharLengthOfEachColumnOfEachQuery.push(maxLengthsOfEachColumn)
@@ -142,80 +140,94 @@ export function activate(context: vscode.ExtensionContext) {
         return res
       }
       for (let queryIndex = 0; queryIndex < asts.length; queryIndex++) {
-        const ast = asts[queryIndex]
-        const _sqls = parser
-          .sqlify(ast, opt)
-          .replace('` (', '`\n(')
-          .replace(') VALUES (', ')\nVALUES\n(')
-          .replace(/\), \(/g, ')\n(')
-          .split('\n')
-        _sqls[1] = _sqls[1].replace(/ /g, '') // remove space from clause of column names
+        const query = asts[queryIndex]
+        if (query.type === 'insert' && query.columns) {
+          const _sqls = parser
+            .sqlify(query, opt)
+            .replace('` (', '`\n(')
+            .replace(') VALUES (', ')\nVALUES\n(')
+            .replace(/\), \(/g, ')\n(')
+            .split('\n')
+          _sqls[1] = _sqls[1].replace(/ /g, '') // remove space from clause of column names
 
-        const sqls = _sqls.map((clause, index) => {
-          if (!clause.startsWith('(')) {
-            return clause
-          }
-
-          // breakpointFinders is a string set to find breaks in value/column-name clauses.
-          // It now supports value types of Varchar, NULL, Boolean, Number, Function.
-          const breakpointFinders = [
-            '(("|\'|`|NULL|TRUE|FALSE|[0-9,-]|\\(\\)),("|\'|`|NULL|TRUE|FALSE|[0-9,-]|[a-zA-Z]*\\(\\)))',
-            '(("|\'|`|NULL|TRUE|FALSE|[0-9,-]|\\(\\))\\))',
-          ]
-          const recordIndex = index > 1 ? index - 2 : index - 1 // consider clauses such as 'INSERT...', 'VALUES...'
-
-          let searchStartPos = 0
-          for (
-            let columnNum = 0;
-            columnNum < maxCharLengthOfEachColumnOfEachQuery[queryIndex].length;
-            columnNum++
-          ) {
-            let graphemePos = 0
-            let insertPos = 0
-            if (
-              columnNum !==
-              maxCharLengthOfEachColumnOfEachQuery[queryIndex].length - 1
-            ) {
-              const re = new RegExp(breakpointFinders[0])
-              graphemePos = clause.substring(searchStartPos).search(re)
-              insertPos =
-                clause.substring(graphemePos + searchStartPos).search(',') +
-                graphemePos +
-                searchStartPos
-            } else {
-              const re = new RegExp(breakpointFinders[1])
-              graphemePos = clause.substring(searchStartPos).search(re)
-              insertPos =
-                clause.substring(graphemePos + searchStartPos).search('\\)') +
-                graphemePos +
-                searchStartPos
+          const sqls = _sqls.map((clause, index) => {
+            if (!clause.startsWith('(')) {
+              return clause
             }
-            const diffOfCharLengthAgainstTheMax =
-              maxCharLengthOfEachColumnOfEachQuery[queryIndex][columnNum] -
-              charLengthsOfEachColumnOfEachQuery[queryIndex][columnNum][
-                recordIndex
-              ]
-            const spaceNumToBeAdded = Math.max(diffOfCharLengthAgainstTheMax, 0)
-            clause = strIns(clause, insertPos, ' '.repeat(spaceNumToBeAdded))
-            searchStartPos = insertPos + 1 + spaceNumToBeAdded
-          }
-          return clause
-        })
 
-        // restructure the query
-        let formattedQuery = sqls.shift()
-        for (let index = 0; index < sqls.length; index++) {
-          const sqlFragment = sqls[index] // sqls = ['(columns)','VALUES','(values1)','(values2)'...] i.e. the 0th element 'INSERT...' was removed by shift()
-          let delimiter = '\n'
-          if (index >= 3) {
-            delimiter = ',\n' // must add colon after records
+            // breakpointFinders is a string set to find breaks in value/column-name clauses.
+            // It now supports value types of Varchar, NULL, Boolean, Number, Function.
+            const breakpointFinders = [
+              '(("|\'|`|NULL|TRUE|FALSE|[0-9,-]|\\(\\)),("|\'|`|NULL|TRUE|FALSE|[0-9,-]|[a-zA-Z]*\\(\\)))',
+              '(("|\'|`|NULL|TRUE|FALSE|[0-9,-]|\\(\\))\\))',
+            ]
+            const recordIndex = index > 1 ? index - 2 : index - 1 // consider clauses such as 'INSERT...', 'VALUES...'
+
+            let searchStartPos = 0
+            for (
+              let columnNum = 0;
+              columnNum <
+              maxCharLengthOfEachColumnOfEachQuery[queryIndex].length;
+              columnNum++
+            ) {
+              let graphemePos = 0
+              let insertPos = 0
+              if (
+                columnNum !==
+                maxCharLengthOfEachColumnOfEachQuery[queryIndex].length - 1
+              ) {
+                const re = new RegExp(breakpointFinders[0])
+                graphemePos = clause.substring(searchStartPos).search(re)
+                insertPos =
+                  clause.substring(graphemePos + searchStartPos).search(',') +
+                  graphemePos +
+                  searchStartPos
+              } else {
+                const re = new RegExp(breakpointFinders[1])
+                graphemePos = clause.substring(searchStartPos).search(re)
+                insertPos =
+                  clause.substring(graphemePos + searchStartPos).search('\\)') +
+                  graphemePos +
+                  searchStartPos
+              }
+              const diffOfCharLengthAgainstTheMax =
+                maxCharLengthOfEachColumnOfEachQuery[queryIndex][columnNum] -
+                charLengthsOfEachColumnOfEachQuery[queryIndex][columnNum][
+                  recordIndex
+                ]
+              const spaceNumToBeAdded = Math.max(
+                diffOfCharLengthAgainstTheMax,
+                0
+              )
+              clause = strIns(clause, insertPos, ' '.repeat(spaceNumToBeAdded))
+              searchStartPos = insertPos + 1 + spaceNumToBeAdded
+            }
+            return clause
+          })
+
+          // restructure the query
+          let formattedQuery = sqls.shift()
+          for (let index = 0; index < sqls.length; index++) {
+            const sqlFragment = sqls[index] // sqls = ['(columns)','VALUES','(values1)','(values2)'...] i.e. the 0th element 'INSERT...' was removed by shift()
+            let delimiter = '\n'
+            if (index >= 3) {
+              delimiter = ',\n' // must add colon after records
+            }
+            formattedQuery = [formattedQuery, sqlFragment].join(delimiter)
           }
-          formattedQuery = [formattedQuery, sqlFragment].join(delimiter)
+          // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+          formattedQuery = formattedQuery?.concat(';') as string
+
+          result.push(formattedQuery)
+        } else {
+          // just add sqlified queries for queries other than 'INSERT'
+          const _sqlifiedQuery = parser.sqlify(query)
+          const sqlifiedQuery = _sqlifiedQuery.endsWith(';')
+            ? _sqlifiedQuery
+            : _sqlifiedQuery.concat(';') // the sqlified result sometimes does not have semi-colon at its end
+
+          result.push(sqlifiedQuery)
         }
-        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-        formattedQuery = formattedQuery?.concat(';') as string
-
-        result.push(formattedQuery)
       }
 
       // insert comments
